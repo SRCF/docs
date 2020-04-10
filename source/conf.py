409650f2e959a77b614ff7649dd8f9901f7ba046
Matias Silva <mw781@cam.ac.uk>
2020-04-10 17:24:39 +0100

    adding theme

--- source/conf.py
+++ source/conf.py
@@ -56,3 +56,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
